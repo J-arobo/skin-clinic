fa86bfefefdb98d9059f71722bb09f399c6faf81
Philip Joseph <arobojoseph4@gmail.com>
2024-04-09 10:06:56 +0300

    menu page change

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let menu = document.querySelector('#menu-btn');
+ /*
+ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
@@ -9,4 +10,27 @@ menu.onclick = () =>{
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     menu.classList.remove('active');
-}  
+} 
+*/
+
+
+/*========= SHOW MENU =========*/
+const navMenu = document.querySelector('.navbar'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*==========MENU SHOW ========*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('active')
+    })
+}
+
+/*==========MENU HIDDEN ========*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('active')
+    })
+}
